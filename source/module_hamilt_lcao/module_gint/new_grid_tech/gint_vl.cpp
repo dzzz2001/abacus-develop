ce5a10d3ab464cd95bdc9cda53f691d063ad46b6
dzzz2001 <dzcpku2001@163.com>
2024-12-25 17:28:39 +0800

    add openmp support to gint_vl

--- source/module_hamilt_lcao/module_gint/new_grid_tech/gint_vl.cpp
+++ source/module_hamilt_lcao/module_gint/new_grid_tech/gint_vl.cpp
@@ -1,5 +1,7 @@
 #include "module_base/array_pool.h"
+#include "module_hamilt_lcao/module_hcontainer/hcontainer.h"
 #include "module_hamilt_lcao/module_hcontainer/hcontainer_funcs.h"
+#include "module_base/blas_connector.h"
 #include "gint_common.h"
 #include "gint_vl.h"
 #include "phi_operator.h"
@@ -26,19 +28,31 @@ void Gint_vl::init_hRGint_()
 
 void Gint_vl::cal_hRGint_()
 {
-    PhiOperator phi_op;
-    for(const auto& biggrid: gint_info_->get_biggrids())
+// be careful!!
+// each thread will have a copy of hRGint_, this may cause a lot of memory usage
+#pragma omp parallel
     {
-        if(biggrid->get_atoms().size() == 0)
+        PhiOperator phi_op;
+        hamilt::HContainer<double> hRGint_local(*hRGint_);
+#pragma omp for schedule(dynamic)
+        for(const auto& biggrid: gint_info_->get_biggrids())
         {
-            continue;
+            if(biggrid->get_atoms().size() == 0)
+            {
+                continue;
+            }
+            phi_op.set_bgrid(biggrid);
+            ModuleBase::Array_Pool<double> phi(phi_op.get_rows(), phi_op.get_cols());
+            ModuleBase::Array_Pool<double> phi_vldr3(phi_op.get_rows(), phi_op.get_cols());
+            phi_op.set_phi(phi.get_ptr_1D());
+            phi_op.phi_mul_vldr3(vr_eff_, dr3_, phi.get_ptr_2D(), phi_vldr3.get_ptr_2D());
+            phi_op.phi_mul_phi_vldr3(phi.get_ptr_2D(), phi_vldr3.get_ptr_2D(), &hRGint_local);
         }
-        phi_op.set_bgrid(biggrid);
-        ModuleBase::Array_Pool<double> phi(phi_op.get_rows(), phi_op.get_cols());
-        ModuleBase::Array_Pool<double> phi_vldr3(phi_op.get_rows(), phi_op.get_cols());
-        phi_op.set_phi(phi.get_ptr_1D());
-        phi_op.phi_mul_vldr3(vr_eff_, dr3_, phi.get_ptr_2D(), phi_vldr3.get_ptr_2D());
-        phi_op.phi_mul_phi_vldr3(phi.get_ptr_2D(), phi_vldr3.get_ptr_2D(), hRGint_.get());
+#pragma omp critical
+        {
+            BlasConnector::axpy(hRGint_local.get_nnr(), 1.0, hRGint_local.get_wrapper(),
+                                1, hRGint_->get_wrapper(), 1);
+        }
     }
 }
 

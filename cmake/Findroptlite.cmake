fa857a8b8cf93ebf3b4b7412c58cb816711e047d
Kai Luo <kluophysics@gmail.com>
2025-07-01 10:39:32 +0800

    Refactor ROPTLITE integration: restore find_package and remove obsolete fetch logic

--- cmake/Findroptlite.cmake
+++ cmake/Findroptlite.cmake
@@ -17,8 +17,13 @@ if(NOT ROPTLITE_DIR)
         URL https://github.com/kluophysics/roptlite/archive/refs/tags/v1.0.0.tar.gz
     )
     FetchContent_MakeAvailable(roptlite)
-    set(ROPTLITE_DIR ${roptlib_SOURCE_DIR})
+    set(ROPTLITE_DIR ${roptlite_SOURCE_DIR})
 endif()
+
+# Handle the QUIET and REQUIRED arguments and set ROPTLITE_FOUND
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(roptlite DEFAULT_MSG ROPTLITE_DIR)
+
 
 # Mark as advanced for cache
 mark_as_advanced(ROPTLITE_DIR)
@@ -28,6 +33,3 @@ mark_as_advanced(ROPTLITE_DIR)
 #     add_subdirectory(${ROPTLITE_DIR} ${CMAKE_BINARY_DIR}/roptlib-build)
 # endif()
 
-# Handle the QUIET and REQUIRED arguments and set ROPTLITE_FOUND
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(roptlite DEFAULT_MSG ROPTLITE_DIR)

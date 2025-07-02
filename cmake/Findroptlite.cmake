ca6678cd982f308fa38738836ddb1cf545522b2f
Kai Luo <kluophysics@gmail.com>
2025-07-02 22:19:50 +0800

    Enhance RDMFT integration: add ENABLE_RDMFT option in CMake, update roptlite path handling in Findroptlite.cmake, and adjust include directories in esolver CMakeLists.txt.

--- cmake/Findroptlite.cmake
+++ cmake/Findroptlite.cmake
@@ -4,9 +4,11 @@
 
 include(FetchContent)
 
-# Try to find an existing ROPTLITE_DIR (with include/roptlite/Problem.h)
+# Try to find an existing ROPTLITE_DIR (with Problems/Problem.h as a marker)
 find_path(ROPTLITE_DIR
-    include/roptlite/Problem.h
+    Problems/Problem.h
+    PATHS ${CMAKE_CURRENT_SOURCE_DIR}/roptlite 
+            ${CMAKE_CURRENT_SOURCE_DIR}/../_deps/
     HINTS ${ROPTLITE_DIR}
 )
 
@@ -14,42 +16,49 @@ if(NOT ROPTLITE_DIR)
     # Download roptlite if not found
     FetchContent_Declare(
         roptlite
-        URL https://github.com/kluophysics/roptlite/archive/refs/tags/v1.0.1.tar.gz
+        GIT_REPOSITORY https://github.com/kluophysics/roptlite.git
+        GIT_TAG "origin/master"
+        GIT_SHALLOW TRUE
+        GIT_PROGRESS TRUE
+        # URL https://github.com/kluophysics/roptlite/archive/refs/tags/v1.0.1.tar.gz
     )
     FetchContent_MakeAvailable(roptlite)
     set(ROPTLITE_DIR ${roptlite_SOURCE_DIR})
+endif()
+
+# Set include directories for roptlite
+set(roptlite_INCLUDE_DIRS
+    ${ROPTLITE_DIR}
+    ${ROPTLITE_DIR}/src
+    ${ROPTLITE_DIR}/src/Manifolds
+    ${ROPTLITE_DIR}/src/Others
+    ${ROPTLITE_DIR}/src/Others/SparseBLAS
+    ${ROPTLITE_DIR}/src/Others/fftw
+    ${ROPTLITE_DIR}/src/Others/wavelet
+    ${ROPTLITE_DIR}/src/Problems
+    ${ROPTLITE_DIR}/src/Solvers
+    ${ROPTLITE_DIR}/src/cwrapper
+    ${ROPTLITE_DIR}/src/cwrapper/blas
+    ${ROPTLITE_DIR}/src/cwrapper/lapack
+    # ${ROPTLITE_DIR}/src/test
+)
+
+# Mark as advanced for cache
+mark_as_advanced(ROPTLITE_DIR)
+
+# Add the roptlite library target if not already present
+if(NOT TARGET roptlite)
+    add_subdirectory(${ROPTLITE_DIR} ${CMAKE_BINARY_DIR}/roptlite-build)
 endif()
 
 # Handle the QUIET and REQUIRED arguments and set ROPTLITE_FOUND
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(roptlite DEFAULT_MSG ROPTLITE_DIR)
 
+# # Export variables for parent scope
+# set(roptlite_INCLUDE_DIRS ${roptlite_INCLUDE_DIRS} PARENT_SCOPE)
+# set(ROPTLITE_DIR ${ROPTLITE_DIR} PARENT_SCOPE)
+
 message(STATUS "Found roptlite: ${ROPTLITE_DIR}")
-message(STATUS "roptlite_INCLUDE_DIR: ${roptlite_INCLUDE_DIR}")
-
-include_directories(
-      ${ROPTLITE_DIR}/
-      ${ROPTLITE_DIR}/src/
-      ${ROPTLITE_DIR}/src/Manifolds
-      ${ROPTLITE_DIR}/src/Others
-      ${ROPTLITE_DIR}/src/Others/SparseBLAS
-      ${ROPTLITE_DIR}/src/Others/fftw
-      ${ROPTLITE_DIR}/src/Others/wavelet
-      ${ROPTLITE_DIR}/src/Problems
-      ${ROPTLITE_DIR}/src/Solvers
-      ${ROPTLITE_DIR}/src/cwrapper
-      ${ROPTLITE_DIR}/src/cwrapper/blas
-      ${ROPTLITE_DIR}/src/cwrapper/lapack
-    #   ${ROPTLITE_DIR}/src/test
-    )
-
-
-
-# Mark as advanced for cache
-mark_as_advanced(ROPTLITE_DIR)
-
-# # Add the roptlite library target if not already present
-# if(NOT TARGET roptlite)
-#     add_subdirectory(${ROPTLITE_DIR} ${CMAKE_BINARY_DIR}/roptlite-build)
-# endif()
+message(STATUS "roptlite_INCLUDE_DIRS: ${roptlite_INCLUDE_DIRS}")
 
